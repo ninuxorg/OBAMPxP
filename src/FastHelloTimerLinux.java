ba63340ef00bfac2ee73942875a4c518f5ee16c0
Fabian Bieker <fb@dinoex.de>
2007-05-24 12:34:22 +0000

    fixed imports
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/OBAMPxP/trunk@69 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- src/FastHelloTimerLinux.java
+++ src/FastHelloTimerLinux.java
@@ -19,17 +19,11 @@
 */
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.util.Enumeration;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
-
-import Multicast.OverlayNeighbor;
 
 
 public class FastHelloTimerLinux {
