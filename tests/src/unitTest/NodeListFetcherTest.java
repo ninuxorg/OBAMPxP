b86f1a98ff2b829b2c749e2a0a0c73a95dd6bf2b
Fabian Bieker <fb@dinoex.de>
2007-05-29 18:36:33 +0000

    added code / dir template to integrate JUnit testing into OBAMPxP. This is all done in trunk/tests to avoid interference with the existing infastructure.
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/OBAMPxP/trunk@134 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ tests/src/unitTest/NodeListFetcherTest.java
@@ -0,0 +1,51 @@
+/*    
+    Copyright (C) 2007 Fabian Bieker
+    This file is part of "Obamp Proxy",
+
+    "Obamp Proxy" is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    "Obamp Proxy" is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with "Obamp Proxy"; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    
+*/
+
+
+
+//import NodeListFetcher; // FIXME: can not import stuff from the default package
+package unitTest;
+
+import junit.framework.TestCase;
+
+/**
+ * JUnit test for NodeListFetcher
+ * @author fb
+ *
+ */
+public class NodeListFetcherTest extends TestCase {
+
+	/**
+	 * Test method for {@link NodeListFetcher#getUrl(DB_Contest)}.
+	 */
+	public void testGetUrlDB_Contest() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link NodeListFetcher#getUrl(java.lang.String)}.
+	 */
+	public void testGetUrlString() {
+		// TODO
+		//	NodeListFetcher.getUrl(
+		//	"http://page.mi.fu-berlin.de/bieker/obamp_nodes_tests.txt");
+	}
+
+}
