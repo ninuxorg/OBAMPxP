31ebe44323387e18f3dd84cb1c7bacc3f58eb7b4
Fabian Bieker <fb@dinoex.de>
2007-06-01 10:39:18 +0000

    cleaned imports
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/OBAMPxP/trunk@146 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- src/DB_Contest.java
+++ src/DB_Contest.java
@@ -17,8 +17,15 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     
 */
-import java.io.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB_Contest{
 	
