b8d8ab8276776747b68f400956aa8ff236984f84
Fabian Bieker <fb@dinoex.de>
2007-06-06 11:41:45 +0000

    fixed indention
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/OBAMPxP/trunk@187 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- src/it/radiolabs/obampxp/IDR_Timer.java
+++ src/it/radiolabs/obampxp/IDR_Timer.java
@@ -26,13 +26,10 @@ import java.util.TimerTask;
 import java.io.DataInputStream;
 import java.net.InetAddress;
 
-
-
-
 public class IDR_Timer {
     
-        Timer timer;
-        Signalling agent;
+	Timer timer;
+	Signalling agent;
 	Integer prova;
 	Stato state;
 	int id;
@@ -41,126 +38,126 @@ public class IDR_Timer {
 	pktTreeCreate pktTC;
 	byte[] pkt_to_send;
 	byte SN;
-	
+
    
     
-    public IDR_Timer(Signalling agent_, InetAddress dest_, int id_, double delay_) {
-		
+	public IDR_Timer(Signalling agent_, InetAddress dest_, int id_, double delay_) {
+
 		agent = agent_;
 		dest = dest_;
 		id = id_;
 		timer = new Timer();
-        timer.schedule(new IDR_TimerTask(), (int)delay_*1000);
-	    
-    }
-    
-    public IDR_Timer(Signalling agent_, DataInputStream is_, int id_, double delay_) {
-		
+		timer.schedule(new IDR_TimerTask(), (int)delay_*1000);
+
+	}
+
+	public IDR_Timer(Signalling agent_, DataInputStream is_, int id_, double delay_) {
+
 		agent = agent_;
 		is = is_;
 		id = id_;
-		
-        timer = new Timer();
-        timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
-	    
-    }
-    
-    public IDR_Timer(Signalling agent_, pktTreeCreate pkt_, int id_, double delay_) {
-		
+
+		timer = new Timer();
+		timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
+
+	}
+
+	public IDR_Timer(Signalling agent_, pktTreeCreate pkt_, int id_, double delay_) {
+
 		agent = agent_;
 		pktTC = pkt_;
 		id = id_;
-		
-        timer = new Timer();
-        timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
-	    
-    }
-    
-    public IDR_Timer(Signalling agent_, byte[] pkt_to_send_, InetAddress dest_, int id_, double delay_) {
-		
+
+		timer = new Timer();
+		timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
+
+	}
+
+	public IDR_Timer(Signalling agent_, byte[] pkt_to_send_, InetAddress dest_, int id_, double delay_) {
+
 		agent = agent_;
 		pkt_to_send = pkt_to_send_;
 		id = id_;
 		dest = dest_;
-				
-        timer = new Timer();
-        timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
-	    
-    }
-    
-    public IDR_Timer(Signalling agent_, int id_, double delay_) {
-		
+
+		timer = new Timer();
+		timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
+
+	}
+
+	public IDR_Timer(Signalling agent_, int id_, double delay_) {
+
 		agent = agent_;
 		id = id_;
-		
-        timer = new Timer();
-        timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
-	    
-    }
-    
-    boolean CHECK_SEQ_NUM(byte stateSN_, byte pktSN_){
-        
-        int stateSN = stateSN_;
-        if(stateSN<0)
-        	stateSN = stateSN + 256;
-        int pktSN = pktSN_;
-        if(pktSN<0)
-        	pktSN = pktSN +256;
-        if (Math.abs(pktSN-stateSN)>5){
-            return true;
-        }else
-            if(stateSN < pktSN){
-                return true;
-            }else
-                return false;
-    }
-    
-    public void handle(){
-    	
-    	
-    	switch (id) {
-    		
-    		
-           	case 2://TreeCreate packet delay
-           		synchronized(agent.signalling_use){
-           			agent.recvTREE_CREATE(pktTC);
-           		}
-           		
-           		break;
-           	case 3://TreeCreateAck Retransmission Timer
-           		OverlayNeighbor nb = agent.neighbors.get(dest);
-           		if (nb == null) break; 
-           		if (CHECK_SEQ_NUM(nb.last_conf_sn, pkt_to_send[2])){
-           			synchronized(agent.signalling_use){
-           				agent.retransmitTreeCreateAck(dest, pkt_to_send);
-           			}	
-    			}
-    			break;
-           	case 4://TreeCreateNack Retransmission Timer
-           		OverlayNeighbor nb1 = agent.neighbors.get(dest);
-           		if (nb1 == null) break; 
-           		if (CHECK_SEQ_NUM(nb1.last_nack_conf_sn, pkt_to_send[2])){
-           			synchronized(agent.signalling_use){
-           				agent.retransmitTREE_CREATE_NACK(dest, pkt_to_send);
-           			}	
-    			} else {
-    				// int i=1; // why? i is never read... (fabian)
-    			}
-    			break;
-           	case 5://expanding ring Hello Timer 
-                        
-                        break;
-           			
-        }    		
-    	
-    }
 
-    class IDR_TimerTask extends TimerTask {
-	
-        @Override
+		timer = new Timer();
+		timer.schedule(new IDR_TimerTask(), (int)delay_*1000);  //subsequent rate
+
+	}
+
+	boolean CHECK_SEQ_NUM(byte stateSN_, byte pktSN_){
+
+		int stateSN = stateSN_;
+		if(stateSN<0)
+			stateSN = stateSN + 256;
+		int pktSN = pktSN_;
+		if(pktSN<0)
+			pktSN = pktSN +256;
+		if (Math.abs(pktSN-stateSN)>5){
+			return true;
+		}else
+			if(stateSN < pktSN){
+				return true;
+			}else
+				return false;
+	}
+
+	public void handle(){
+
+
+		switch (id) {
+
+
+		case 2://TreeCreate packet delay
+			synchronized(agent.signalling_use){
+				agent.recvTREE_CREATE(pktTC);
+			}
+
+			break;
+		case 3://TreeCreateAck Retransmission Timer
+			OverlayNeighbor nb = agent.neighbors.get(dest);
+			if (nb == null) break; 
+			if (CHECK_SEQ_NUM(nb.last_conf_sn, pkt_to_send[2])){
+				synchronized(agent.signalling_use){
+					agent.retransmitTreeCreateAck(dest, pkt_to_send);
+				}	
+			}
+			break;
+		case 4://TreeCreateNack Retransmission Timer
+			OverlayNeighbor nb1 = agent.neighbors.get(dest);
+			if (nb1 == null) break; 
+			if (CHECK_SEQ_NUM(nb1.last_nack_conf_sn, pkt_to_send[2])){
+				synchronized(agent.signalling_use){
+					agent.retransmitTREE_CREATE_NACK(dest, pkt_to_send);
+				}	
+			} else {
+				// int i=1; // why? i is never read... (fabian)
+			}
+			break;
+		case 5://expanding ring Hello Timer 
+
+			break;
+
+		}    		
+
+	}
+
+	class IDR_TimerTask extends TimerTask {
+
+		@Override
 		public void run() {
-	    		handle();
-	    		timer.cancel();
-        }
-    } 
+			handle();
+			timer.cancel();
+		}
+	} 
 } 
