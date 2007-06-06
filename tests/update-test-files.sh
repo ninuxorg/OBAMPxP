a3693f57835f4783751861ba1af081eca1b30ac0
Fabian Bieker <fb@dinoex.de>
2007-06-06 19:45:21 +0000

    updated
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/OBAMPxP/trunk@193 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- tests/update-test-files.sh
+++ tests/update-test-files.sh
@@ -4,7 +4,6 @@ cd "$(dirname $0)"
 cp ../dist/OBAMPxP.jar node-olsr-fb/
 cp ../dist/obamp.sh node-olsr-fb/
 chmod 755 node-olsr-fb/obamp.sh
-mkdir -p node-olsr-fb/lib/ node-olsr-fb/image/
+mkdir -p node-olsr-fb/image/
 cp ../log4j.properties node-olsr-fb/
-cp -R ../lib/* node-olsr-fb/lib/
 cp -R ../image/* node-olsr-fb/image/
